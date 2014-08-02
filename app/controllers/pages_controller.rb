9d4c9e128f151130900f8bdfb6781f7d6149a91d
Alex Pinsky <alex_pinsky85@yahoo.com>
2014-08-02 15:31:40 +0300

    finish chapter 2

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,16 @@
 class PagesController < ApplicationController
-  
+
   def index
     render layout: false
+  end
+
+  def items
+    items = [
+      { action: "Buy Flowers", done: false },
+      { action: "Get Shoes", done: false },
+      { action: "Collect Tickets", done: true },
+      { action: "Call Joe", done: false }
+    ]
+    render json: items
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Angshake::Application.routes.draw do
   root 'pages#index'
+  get 'items' => 'pages#items'
 end
